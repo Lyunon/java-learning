1a00dea06e4e079719381728fa7086a160311823
Geonho Yoo <ygh7820@naver.com>
2023-08-30 23:34:48 +0900

    Create byte.java

---
+++ #001. 변수와 타입/example/byte.java
@@ -0,0 +1,16 @@
+public class byte{
+        public static void main(String[] args) {
+            byte var1 = -128;
+            byte var2 = -30;
+            byte var3 = 0;
+            byte var4 = 30;
+            byte var5 = 127;
+            byte var6 = 128; //byte 타입의 값의 허용 범위는 127까지이기에 컴파일 에러
+            
+            System.out.println(var1);
+            System.out.println(var2);
+            System.out.println(var3);
+            System.out.println(var4);
+            System.out.println(var5);
+    }
+}
