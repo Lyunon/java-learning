8d4d68a72243a0ce0b8b4b404b4a3096243b7f55
Geonho Yoo <ygh7820@naver.com>
2023-08-30 23:41:27 +0900

    Create Long.java

---
+++ #001. 변수와 타입/example/Long.java
@@ -0,0 +1,12 @@
+public class Long{
+        public static void main(String[] args) {
+            long var1 = 10;
+            long var2 = 20L;
+            //long var3 = 1000000000000; 범위 밖이기에 주석 제거 시 컴파일 에러
+            long var4= 1000000000000L;
+            
+            System.out.println(var1);
+            System.out.println(var2);
+            System.out.println(var4);
+    }
+}
