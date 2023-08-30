0526c7a2eaf9fb87de16b7bd31bf4f1c201aba2e
Geonho Yoo <ygh7820@naver.com>
2023-08-30 22:50:53 +0900

    Update VariableInitialization.java

--- #001. 변수와 타입/example/VariableInitialization.java
+++ #001. 변수와 타입/example/VariableInitialization.java
@@ -1,14 +1,8 @@
-package sec01.exam01;
-
-public class VariableInitialization {
-  public static void main(String[] args){
-    //value라는 변수 선언함.
-    int value;
-
-    //value라는 변수 값을 읽고 10을 더하는 산술 연산 수행, 연산의 결과값을 result라는 변수의 초기값으로 저장함.
-    int result = value + 10;
-
-    //result라는 변수 값을 읽고 콘솔에 출력함.
-    System.out.println(result);
+public class VariableInitialization{
+      public static void main(String[] args) {
+            int value;
+            int result = value + 10;
+            System.out.println(result);
   }
 }
+//변수가 초기화되지 않아 컴파일 에러가 발생함.
