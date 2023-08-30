33e5c2df3085389c6411aa0f9f7c07a4268ae700
Geonho Yoo <ygh7820@naver.com>
2023-08-30 22:08:26 +0900

    Create VariableInitialization.java

---
+++ #001. 변수와 타입/example/VariableInitialization.java
@@ -0,0 +1,14 @@
+package sec01.exam01;
+
+public class VariableInitialization {
+  public static void main(String[] args){
+    //value라는 변수 선언함.
+    int value;
+
+    //value라는 변수 값을 읽고 10을 더하는 산술 연산 수행, 연산의 결과값을 result라는 변수의 초기값으로 저장함.
+    int result = value + 10;
+
+    //result라는 변수 값을 읽고 콘솔에 출력함.
+    System.out.println(result);
+  }
+}
