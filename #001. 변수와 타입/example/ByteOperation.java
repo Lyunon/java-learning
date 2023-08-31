8454a528c7b75ac44aaef442d7763cf7f54ce234
Geonho Yoo <ygh7820@naver.com>
2023-08-31 22:53:39 +0900

    Create ByteOperation.java

---
+++ #001. 변수와 타입/example/ByteOperation.java
@@ -0,0 +1,11 @@
+public class ByteOperation{
+    public static void main(String[] args){
+        byte result1 = 10 + 20;
+        System.out.println(result1);
+        
+        byte x = 10;
+        byte y = 20;
+        int result2 = x + y;
+        System.out.println(result2);
+    }
+}
