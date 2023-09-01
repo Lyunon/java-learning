9f614f24eaa1bf75f39f4ef62c4d259c24169ee0
Geonho Yoo <ygh7820@naver.com>
2023-09-01 22:45:28 +0900

    Create KeyCode.java

---
+++ #001. 변수와 타입/example/KeyCode.java
@@ -0,0 +1,14 @@
+public class KeyCode{
+    public static void main(String[] args) throws Exception {
+        int keyCode;
+
+        keyCode = System.in.read();
+        System.out.println("keyCode: " + keyCode);
+
+        keyCode = System.in.read();
+        System.out.println("keyCode: " + keyCode);
+
+        keyCode = System.in.read();
+        System.out.println("keyCode: " + keyCode);
+    }
+}
