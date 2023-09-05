0a2b892e49b7b97ec2965d70481a44828ed09af7
Geonho Yoo <ygh7820@naver.com>
2023-09-05 23:19:14 +0900

    Create ConditionOperation.java

---
+++ #002. 연산자/example/ConditionOperation.java
@@ -0,0 +1,7 @@
+public class ConditionOperation {
+    public static void main(String[] args) {
+        int score = 85;
+        char grade = (score > 90) ? 'A' : ( (score > 80) ? 'B' : 'C' );
+        System.out.println(score + "점은 " + grade + "등급입니다.");
+    }
+}
