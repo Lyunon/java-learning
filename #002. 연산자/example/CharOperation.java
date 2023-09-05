a87f3b830f1ca0b9f4cc5b8cf22960ca2806fae7
Geonho Yoo <ygh7820@naver.com>
2023-09-05 22:30:01 +0900

    Create CharOperation.java

---
+++ #002. 연산자/example/CharOperation.java
@@ -0,0 +1,10 @@
+public class CharOperation {
+    public static void main(String[] args) {
+        char c1 = 'A' + 1; //char c1=bb;로 컴파일됨.
+        char c2 = 'A';
+        //char c3 = c2 + 1; char 변수가 산술 연산에 사용도면 int 타입으로 변환되므로 연산 결과는 int 타입이 됨.
+        System.out.println("c1: " + c1);
+        System.out.println("c2: " + c2);
+        //System.out.println("c3: " + c3);
+    }
+}
