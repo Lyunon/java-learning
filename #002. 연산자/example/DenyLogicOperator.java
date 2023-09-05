53ed18331f5f6ac02893f7cae0a54759cb8df6b0
Geonho Yoo <ygh7820@naver.com>
2023-09-05 22:19:12 +0900

    Create DenyLogicOperator.java

---
+++ #002. 연산자/example/DenyLogicOperator.java
@@ -0,0 +1,12 @@
+public class DenyLogicOperator {
+    public static void main(String[] args) {
+        boolean play = true;
+        System.out.println(play);
+
+        play = !play;
+        System.out.println(play);
+
+        play = !play;
+        System.out.println(play);
+    }
+}
