cad0e27a8a70ee8af7464ebe00942bf93051c547
Geonho Yoo <ygh7820@naver.com>
2023-09-05 23:34:45 +0900

    Create IfElse.java

---
+++ #003. 조건문과 반복문/example/IfElse.java
@@ -0,0 +1,13 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int score = 85;
+
+        if(score>=90) {
+            System.out.println("점수가 90보다 큽니다.");
+            System.out.println("등급은 A입니다.");
+        } else { //score < 90인 경우
+            System.out.println("점수가 90보다 작습니다.");
+            System.out.println("등급은 B입니다.");
+        }
+    }
+}
