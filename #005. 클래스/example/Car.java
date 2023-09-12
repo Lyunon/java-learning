425599887081a703d3ea72d8e0fbea16340c07db
Geonho Yoo <ygh7820@naver.com>
2023-09-12 23:20:06 +0900

    Update Car.java

--- #005. 클래스/example/Car.java
+++ #005. 클래스/example/Car.java
@@ -5,10 +5,10 @@ public class Car{
         System.out.println("제작회사: " + myCar.company);
         System.out.println("모델명: " + myCar.model);
         System.out.println("색깔: " + myCar.color);
-        System.out.println("최고속도: " + myCar.MaxSpeed);
+        System.out.println("최고속도: " + myCar.maxSpeed);
         System.out.println("현재속도: " + myCar.speed);
 
         myCar.speed = 60;
-        System.out.println("수정된 속도:" + myCar.speed)
+        System.out.println("수정된 속도: " + myCar.speed);
     }
 }
