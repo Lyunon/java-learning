ea5976a69a312b1461706afaa87e06e11ab932c5
Geonho Yoo <ygh7820@naver.com>
2023-09-15 23:36:58 +0900

    Create Korean.java

---
+++ #005. 클래스/example/Korean.java
@@ -0,0 +1,11 @@
+public class Korean {
+    public static void main(String[] args) {
+        KoreanAPI k1 = new Korean("박자바", "011225-1234567");
+        System.out.println("k1.name : " + k1.name);
+        System.out.println("k1.ssn : " + k1.ssn);
+
+        KoreanAPI k2 = new Korean("김자바", "930525-0654321");
+        System.out.println("k2.name : " + k2.name);
+        System.out.println("k2.ssn : " + k2.ssn);
+    }
+}
