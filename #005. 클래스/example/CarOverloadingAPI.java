480847c4dc17884ae97963477dc5efb5e936ff2c
Geonho Yoo <ygh7820@naver.com>
2023-09-15 23:49:09 +0900

    Create CarOverloadingAPI.java

---
+++ #005. 클래스/example/CarOverloadingAPI.java
@@ -0,0 +1,26 @@
+public class CarOverloadingAPI {
+    //필드
+    String company = "현대자동차";
+    String model;
+    String color;
+    int maxSpeed;
+
+    //생성자
+    Car() {
+    }
+
+    Car(String model) {
+        this.model = model;
+    }
+
+    Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    Car(String model, String color, int maxSpeed) {
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+}
