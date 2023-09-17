9856ea1f022ecfc42e279f55cf1471db1ee5089f
Geonho Yoo <ygh7820@naver.com>
2023-09-17 22:43:31 +0900

    Create ComputerAPI.java

---
+++ #005. 클래스/example/ComputerAPI.java
@@ -0,0 +1,17 @@
+public class ComputerAPI { 
+    int sum1(int[] values) {
+        int sum = 0;
+        for(int i=0; i<values.length; i++) {
+            sum += values[i];
+        }
+        return sum;
+    }
+
+    int sum2(int ... values) {
+        int sum = 0;
+        for(int i=0; i<values.length; i++) {
+            sum += values[i];
+        }
+        return sum;
+    }
+}
