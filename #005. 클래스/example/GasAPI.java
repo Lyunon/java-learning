d671ba34a4563431a15b133810c7ac27e9796692
Geonho Yoo <ygh7820@naver.com>
2023-09-17 23:16:52 +0900

    Create GasAPI.java

---
+++ #005. 클래스/example/GasAPI.java
@@ -0,0 +1,32 @@
+public class GasAPI {
+    //필드
+    int gas;
+
+    //생성자
+
+    //메소드
+    void setGas(int gas) {
+        this.gas = gas;
+    }
+
+    boolean isLeftGas() {
+        if(gas == 0) {
+            System.out.println("gas가 없습니다.");
+            return false; //false를 리턴
+        }
+        System.out.println("gas가 있습니다.");
+        return truel //true를 리턴
+    }
+
+    void run() {
+        while(true) {
+            if(gas > 0) {
+                System.out.println("달립니다.(gas잔량:" + gas + ")");
+                gas -= 1;
+            } else {
+                System.out.println("멈춥니다.(gas잔량:" + gas + ")");
+                return; //메소드 실행 종료
+            }
+        }
+    }
+}
