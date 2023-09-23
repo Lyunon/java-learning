732bcaecaa00a6c19f06cc56668f23bdd5a2e9f9
Geonho Yoo <ygh7820@naver.com>
2023-09-23 23:12:08 +0900

    Create CarThis2.java

---
+++ #005. 클래스/example/CarThis2.java
@@ -0,0 +1,9 @@
+public class CarThis2 {
+    public static void main(String[] args) {
+        CarThis2API myCar = new Car("포르쉐");
+        CarThis2API yourCar = new Car("벤츠");
+
+        myCar.run();
+        yourCar.run();
+    }
+}
