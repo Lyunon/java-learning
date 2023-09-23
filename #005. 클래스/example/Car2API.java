48aa86084728f4db59d86fc4d14675775fb506c3
Geonho Yoo <ygh7820@naver.com>
2023-09-23 22:20:43 +0900

    Create Car2API.java

---
+++ #005. 클래스/example/Car2API.java
@@ -0,0 +1,22 @@
+package class Car2API {
+    //필드
+    int speed;
+
+    //생성자
+
+    //메소드
+    int getSpeed() {
+        return speed;
+    }
+
+    void keyTurnOn() {
+        System.out.println("키를 돌립니다.");
+    }
+
+    void run() {
+        for(int i=10; i<=50l i=+10) {
+            speed = i;
+            System.out.println("달립니다. (시속:" + speed + "km/h)");
+        }
+    }
+}
