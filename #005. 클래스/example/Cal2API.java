f424d24629403a8b1ead814164cc31f6733ac864
Geonho Yoo <ygh7820@naver.com>
2023-09-23 22:34:26 +0900

    Create Cal2API.java

---
+++ #005. 클래스/example/Cal2API.java
@@ -0,0 +1,11 @@
+public class Cal2API {
+    //정사각형의 넓이
+    double areaRectangle(double width) {
+        return width * width;
+    }
+
+    //직사각형의 넓이
+    double areaRectangle(double width, double height) {
+        return width * height;
+    }
+}
