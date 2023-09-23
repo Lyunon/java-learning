0458cf5cfc3033083636bb3708be616706a1b4a8
Geonho Yoo <ygh7820@naver.com>
2023-09-24 00:10:45 +0900

    Create Earth.java

---
+++ #005. 클래스/example/Earth.java
@@ -0,0 +1,6 @@
+public class Earth {
+    public static void main(String[] args) {
+        System.out.println("지구의 반지름: " + EarthAPI.EARTH_RADIUS + " km");
+        System.out.println("지구의 표면적: " + EarthAPI.EARTH_AREA + " km^2");
+    }
+}
