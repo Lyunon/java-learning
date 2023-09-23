cbfc7853cb0657f813b60c7d377b6fce414ed243
Geonho Yoo <ygh7820@naver.com>
2023-09-23 23:23:05 +0900

    Create CalStaticAPI.java

---
+++ #005. 클래스/example/CalStaticAPI.java
@@ -0,0 +1,11 @@
+public class CalStaticAPI {
+    static double pi = 3.14159;
+
+    static int plus(int x, int y) {
+        return x + y;
+    }
+
+    static int minus(int x, int y) {
+        return x - y;
+    }
+}
