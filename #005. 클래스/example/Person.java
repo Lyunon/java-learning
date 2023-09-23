8702b911e41b668ba4d39402bd695f4972ba0b1e
Geonho Yoo <ygh7820@naver.com>
2023-09-24 00:00:14 +0900

    Create Person.java

---
+++ #005. 클래스/example/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    public static void main(String[] args) {
+        PersonAPI p1 = new Person("123456-1234567", "홍길동");
+
+        System.out.println(p1.nation);
+        System.out.println(p1.ssn);
+        System.out.println(p1.name);
+
+        //p1.nation = "usa";
+        //p1.ssn = "654321-7654321";
+        p1.name = "홍삼원";
+    }
+}
