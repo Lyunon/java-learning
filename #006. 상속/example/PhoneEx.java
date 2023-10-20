a62d4fb98408de7edc0c7fcbf348a0136b135877
Geonho Yoo <ygh7820@naver.com>
2023-10-20 23:30:44 +0900

    Create PhoneEx.java

---
+++ #006. 상속/example/PhoneEx.java
@@ -0,0 +1,11 @@
+public class PhoneEx {
+    public static void main(String[] args) {
+        //Phone phone = new phone();
+
+        SmartPhone smartPhone = new SmartPhone("홍길동");
+
+        smartPhone.turnOn();
+        smartPhone.internetSearch();
+        smartPhone.turnOff();
+    }
+}
