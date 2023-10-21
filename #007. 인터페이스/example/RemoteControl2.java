2b993c611291d6eaa7a40904405fefa200a01cb2
Geonho Yoo <ygh7820@naver.com>
2023-10-21 22:52:08 +0900

    Create RemoteControl2.java

---
+++ #007. 인터페이스/example/RemoteControl2.java
@@ -0,0 +1,10 @@
+public interface RemoteControl2 {
+    //상수
+    public int MAX_VOLUME = 10;
+    public int MIN_VOLUME = 0;
+
+    //추상 메소드
+    public void turnOn();
+    public void turnOff();
+    public void setVolume(int volume);
+}
