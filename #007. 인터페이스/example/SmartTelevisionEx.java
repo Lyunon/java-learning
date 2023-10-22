2a2818d07425912dba90535a51b18e5d9b0a4d45
Geonho Yoo <ygh7820@naver.com>
2023-10-22 23:07:36 +0900

    Create SmartTelevisionEx.java

---
+++ #007. 인터페이스/example/SmartTelevisionEx.java
@@ -0,0 +1,8 @@
+public class SmartTelevisionEx {
+    public static void main(String[] args) {
+        SmartTelevision tv = new SmartTelevision();
+
+        RemoteControl rc = tv;
+        Searchable searchable = tv;
+    }
+}
