344e1fd3e232e610e29e866f53f3611d61741deb
Geonho Yoo <ygh7820@naver.com>
2023-10-28 23:20:39 +0900

    Create OutterEx.java

---
+++ #008. 중첩 클래스와 중첩 인터페이스/example/OutterEx.java
@@ -0,0 +1,7 @@
+public class OutterEx {
+    public static vodi main(String[] args) {
+        Outter2 outter = new Outter();
+        Outter2.Nested nested = outter.new Nested();
+        nested.print();
+    }
+}
