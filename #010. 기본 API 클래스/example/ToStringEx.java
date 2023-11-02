fb1a2a07b7ac8e96aefcc41fbd6c52cb1dde429e
Geonho Yoo <ygh7820@naver.com>
2023-11-02 23:26:49 +0900

    Create ToStringEx.java

---
+++ #010. 기본 API 클래스/example/ToStringEx.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class ToStringEx {
+    public static void main(String[] args) {
+        Object obj1 = new Object();
+        Date obj2 = new Date();
+        System.out.println(obj1.toString());
+        System.out.println(obj2.toString());
+    }
+}
