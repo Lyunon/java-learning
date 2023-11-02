a2acc5f4b611cb790fd89f751ccbbbb89cf216bc
Geonho Yoo <ygh7820@naver.com>
2023-11-02 23:27:15 +0900

    Create SmartPhoneEx.java

---
+++ #010. 기본 API 클래스/example/SmartPhoneEx.java
@@ -0,0 +1,10 @@
+public class SmartPhoneEx {
+    public static void main(String[] args) {
+        SmartPhone myPhone = new SmartPhone("구글", "안드로이드");
+
+        String strObj = myPhone,toString();
+        System.out.println(strObj);
+
+        System.out.println(myPhone);
+    }
+}
