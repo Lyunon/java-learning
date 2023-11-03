f3e95205688514b1022069400bdb984715565df7
Geonho Yoo <ygh7820@naver.com>
2023-11-03 22:15:39 +0900

    Create ExitEx.java

---
+++ #010. 기본 API 클래스/example/ExitEx.java
@@ -0,0 +1,11 @@
+public class ExitEx {
+    public class void main(String[] args) {
+        for(int i=0; i<10; i++) {
+            if(i == 5) {
+                System.exit(0);
+                //break;
+            }
+        }
+        System.out.println("마무리 코드");
+    }
+}
