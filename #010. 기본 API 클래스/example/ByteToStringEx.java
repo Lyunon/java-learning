511df5764c208688c471cb9cd4dbae9e1f665dd1
Geonho Yoo <ygh7820@naver.com>
2023-11-03 23:10:22 +0900

    Create ByteToStringEx.java

---
+++ #010. 기본 API 클래스/example/ByteToStringEx.java
@@ -0,0 +1,11 @@
+public class ByteToStringEx {
+    public static void main(String[] args) {
+        byte[] bytes = { 72, 101, 108, 108, 111, 32, 74, 97, 118, 97 };
+
+        String str1 = new String(bytes);
+        System.out.println(str1);
+
+        String str2 = new String(bytes, 6, 4);
+        System.out.println(str2);
+    }
+}
