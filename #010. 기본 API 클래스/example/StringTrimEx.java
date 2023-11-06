7f6f84d7eb173f7b360c8c13fc6200ac2707109e
Geonho Yoo <ygh7820@naver.com>
2023-11-06 23:16:14 +0900

    Create StringTrimEx.java

---
+++ #010. 기본 API 클래스/example/StringTrimEx.java
@@ -0,0 +1,9 @@
+public class StringTrimEx {
+    public static void main(String[] args) {
+        String tel = " 02";
+        String tel2 = "123 ";
+        String tel3 = " 1234 ";
+
+        String tel = tel1.trim() + tel2.trim() + tel3.trim();
+    }
+}
