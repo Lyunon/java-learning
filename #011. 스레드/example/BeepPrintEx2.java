0b85833d6a069c4d5369ff226290d9fa89f58e9a
Geonho Yoo <ygh7820@naver.com>
2023-11-09 23:30:32 +0900

    Create BeepPrintEx2.java

---
+++ #011. 스레드/example/BeepPrintEx2.java
@@ -0,0 +1,13 @@
+public class BeepPrintEx2 {
+    public static void main(String[] args) {
+        Runnable beepTask = new BeepTask();
+        Thread thread = new Thread(beepTask);
+        thread.start();
+
+        for(int i=0; i<5; i++) {
+            System.out.println("띵");
+            try {Thread.sleep(500);}
+            catch(Exceptoin e) {}
+        }
+    }
+}
