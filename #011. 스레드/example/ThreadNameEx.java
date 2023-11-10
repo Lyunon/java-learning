9e4b0635a3c5a4477acb89cc867dd7c541bd4dfd
Geonho Yoo <ygh7820@naver.com>
2023-11-10 22:18:15 +0900

    Create ThreadNameEx.java

---
+++ #011. 스레드/example/ThreadNameEx.java
@@ -0,0 +1,14 @@
+public class ThreadNameEx {
+    public static void main(String[] args) {
+        Thread mainThread = Thread.currentThread();
+        System.out.println("프로그램 시작 스레드 이름: " + mainThread.getName());
+
+        ThreadA threadA = new ThreadA();
+        System.out.println("작업 스레드 이름: " + threadA.getName());
+        threadA.start();
+
+        ThreadB threadB = new ThreadB();
+        System.out.println("작업 스레드 이름: " + threadB.getName());
+        threadB.start();
+    }
+}
