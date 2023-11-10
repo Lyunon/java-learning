91a5c561feaa011103fb08b601ca795e87136738
Geonho Yoo <ygh7820@naver.com>
2023-11-10 23:06:38 +0900

    Create SleepEx.java

---
+++ #011. 스레드/example/SleepEx.java
@@ -0,0 +1,14 @@
+import java.awt.Toolkit;
+
+public class SleepEx {
+    public static void main(String[] args) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        for(int i=0; i<10; i++) {
+            toolkit.beep();
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {}
+            
+        }
+    }
+}
