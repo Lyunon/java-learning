a977198b886248fa5e88755390edbea35a827c60
Geonho Yoo <ygh7820@naver.com>
2023-11-10 22:10:52 +0900

    Create BeepPrintEx4.java

---
+++ #011. 스레드/example/BeepPrintEx4.java
@@ -0,0 +1,11 @@
+public class BeepPrintEx4 {
+    public static void main(String[] args) {
+        Thread thread = new BeepThread();
+        thread.start();
+
+        for(int i=0; i<5; i++) {
+            System.out.println("띵");
+            try{Thread.sleep(500);} catch(Exception e) {}
+        }
+    }
+}
