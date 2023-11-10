75c8f5c4ba40f5d36f3b099af79436a72e4a68a7
Geonho Yoo <ygh7820@naver.com>
2023-11-10 23:17:17 +0900

    Create StopFlagEx.java

---
+++ #011. 스레드/example/StopFlagEx.java
@@ -0,0 +1,10 @@
+public class StopFlagEx {
+    public static void main(String[] args) {
+        PrintThread1 printThread = new PrintThread1();
+        printThread.start();
+
+        try {Thread.sleep(1000);} catch (InterruptedException e) {}
+
+        printThread.setStop(true);
+    }
+}
