e805b8fb219137dac2b00780d3b9513a30166ee2
Geonho Yoo <ygh7820@naver.com>
2023-11-10 23:22:08 +0900

    Create InterruptEx.java

---
+++ #011. 스레드/example/InterruptEx.java
@@ -0,0 +1,10 @@
+public class InterruptEx {
+    public static void main(String[] args) {
+        Thread thread = new PrintThread2();
+        thread.start();
+
+        try {Thread.sleep(1000);} catch (InterruptedException e) {}
+
+        thread.interrupt();
+    }
+}
