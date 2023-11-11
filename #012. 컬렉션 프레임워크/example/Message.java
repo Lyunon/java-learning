d6ce44286565a66a3ed3a9aa7351733fd0720424
Geonho Yoo <ygh7820@naver.com>
2023-11-12 00:02:35 +0900

    Create Message.java

---
+++ #012. 컬렉션 프레임워크/example/Message.java
@@ -0,0 +1,9 @@
+public class Message {
+    public String command;
+    public String to;
+
+    public Message(String command, String to) {
+        this.command = command;
+        this.to = to;
+    }
+}
