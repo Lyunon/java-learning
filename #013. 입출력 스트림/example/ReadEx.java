d0a3d540c4a7889cac7d6508a872841b5df50545
Geonho Yoo <ygh7820@naver.com>
2023-11-13 22:21:30 +0900

    Create ReadEx.java

---
+++ #013. 입출력 스트림/example/ReadEx.java
@@ -0,0 +1,14 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class ReadEx {
+    public static void main(String[] args) throws Exception {
+        InputStream is = new FileInputStream("C:/Temp/test1.db");
+        while(true) {
+            int data = is.read();
+            if(data == -1) break;
+            System.out.println(data);
+        }
+        is.close();
+    }
+}
