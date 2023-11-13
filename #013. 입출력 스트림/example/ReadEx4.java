07fa38562749b84fc3eefbb9fc255acf6464d4ff
Geonho Yoo <ygh7820@naver.com>
2023-11-13 22:56:16 +0900

    Create ReadEx4.java

---
+++ #013. 입출력 스트림/example/ReadEx4.java
@@ -0,0 +1,14 @@
+import java.io.FileReader;
+import java.io.Reader;
+
+public class ReadEx4 {
+    public static void main(String[] args) throws Exception {
+        Reader reader = new FileReader("C:/Temp/test7.txt");
+        while(true) {
+            int data = reader.read();
+            if(data == -1) break;
+            System.out.println((char)data);
+        }
+        reader.close();
+    }
+}
