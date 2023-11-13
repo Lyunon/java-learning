23df04adec1225b52060f9118cc133526cc64370
Geonho Yoo <ygh7820@naver.com>
2023-11-13 23:02:29 +0900

    Create ReadEx5.java

---
+++ #013. 입출력 스트림/example/ReadEx5.java
@@ -0,0 +1,20 @@
+import java.io.FileReader;
+import java.io.Reader;
+
+public class ReadEx5 {
+    public static void main(String[] args) throws Exception {
+        Reader reader = new FileReader("C:/Temp/test8.txt");
+
+        char[] buffer = new char[100];
+
+        while(true) {
+            int readCharNum = reader.read(buffer);
+            if(readCharNum == -1) break;
+            for(int i=0; i<readCharNum; i++) {
+                System.out.println(buffer[i]);
+            }
+        }
+        
+        reader.close();
+    }
+}
