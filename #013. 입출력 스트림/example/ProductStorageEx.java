e8a22c995f6efd984bf8447a50fe834a2d80d297
Geonho Yoo <ygh7820@naver.com>
2023-12-01 23:43:34 +0900

    Create ProductStorageEx.java

---
+++ #013. 입출력 스트림/example/ProductStorageEx.java
@@ -0,0 +1,6 @@
+public class ProductStorageEx {
+    public static void main(String[] args) {
+        ProductStorage productStorage = new ProductStorage();
+        productStorage.showMenu();
+    }
+}
