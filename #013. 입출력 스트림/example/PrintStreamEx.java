ca0d0db5e097f44431c70e45ab4f3ddf2c523082
Geonho Yoo <ygh7820@naver.com>
2023-12-01 22:13:15 +0900

    Create PrintStreamEx.java

---
+++ #013. 입출력 스트림/example/PrintStreamEx.java
@@ -0,0 +1,17 @@
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+pubilc class PrintStreamEx {
+    public static void main(String[] args) throws Exception {
+        FileOutputStream fos = new FileOutputStream("C:/Temp/printstream.txt");
+        PrintStream ps = new PrintStream(fos);
+
+        ps.println("[프린터 보조 스트림]");
+        ps.print("마치 ");
+        ps.println("프린터가 출력하는 것처럼 ");
+        ps.println("데이터를 출력합니다.");
+
+        ps.flush();
+        ps.close();
+    }
+}
