1a565e92db8b0d92479366c06409c35d9e53aeec
Geonho Yoo <ygh7820@naver.com>
2023-12-01 22:39:37 +0900

    Update PrintStreamEx.java

--- #013. 입출력 스트림/example/PrintStreamEx.java
+++ #013. 입출력 스트림/example/PrintStreamEx.java
@@ -1,7 +1,7 @@
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
-pubilc class PrintStreamEx {
+public class PrintStreamEx {
     public static void main(String[] args) throws Exception {
         FileOutputStream fos = new FileOutputStream("C:/Temp/printstream.txt");
         PrintStream ps = new PrintStream(fos);
